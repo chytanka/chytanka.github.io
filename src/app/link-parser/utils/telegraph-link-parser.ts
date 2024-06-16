2c6902eb43edecbe6b5319c5dbe157222b0e10e0
Andrii Rodzyk <rodzyk@programmer.net>
2024-06-16 19:02:24 +0300

    fix: telegraph link

--- src/app/link-parser/utils/telegraph-link-parser.ts
+++ src/app/link-parser/utils/telegraph-link-parser.ts
@@ -2,6 +2,6 @@ import { LinkParser } from "./link-parser";
 
 export class TelegraphLinkParser extends LinkParser {
     // override regex = /telegra\.ph\/([\w\-_іїґ]+)/;
-    override regex = /telegra\.ph\/([\p{L}\p{N}\p{M}\-_]+)/u;
+    override regex = /telegra\.ph\/([\p{L}\p{N}\p{M}\-_%]+)/u;
     override site = 'telegr'
 };

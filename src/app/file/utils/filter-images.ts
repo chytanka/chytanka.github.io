3672f36e6e513e839dac4c906ce40f3ac2406798
Andrii Rodzyk <rodzyk@programmer.net>
2026-03-29 16:19:54 +0300

    feat(zip/cbz): enhance image filtering and processing for SVG files

--- src/app/file/utils/filter-images.ts
+++ src/app/file/utils/filter-images.ts
@@ -1,5 +1,5 @@
 export function filterImages(fileList: Array<string>) {
-    const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp'];
+    const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp', '.tiff', '.svg', '.svgz'];
 
     return fileList.filter(file => {
         const extension = file.substring(file.lastIndexOf('.')).toLowerCase();

81b78072fedfd595adcb7ac8ac8b1f4533c5d74c
Andrii Rodzyk <rodzyk@programmer.net>
2026-04-09 23:17:42 +0300

    chore(release): update version to 0.13.56 in package.json and environment files

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 const PROXY = `http://192.168.10.107:3003/api?url=`
 
 export const environment = {
-    version: "0.13.54-2026.4.8",
+    version: "0.13.56-2026.4.9",
     prod: false,
     proxy: PROXY,
     blankaryoHost: `https://blankary.com/page/`,
